01742347bf8853841de0fdc70ea6d754b75cb668
simongarisch <gatman946@gmail.com>
2019-12-10 13:51:06 +1100

    loading to pypi

---
+++ pyPI load.bat
@@ -0,0 +1,14 @@
+call activate py27
+rem call activate py36
+
+rem make sure we are up to date
+python -m pip install --user --upgrade setuptools wheel
+python -m pip install --upgrade twine
+
+rem now build
+python setup.py sdist bdist_wheel
+
+rem and push
+python -m twine upload dist/*
+
+pause
--- setup.py
+++ setup.py
@@ -4,16 +4,10 @@ from setuptools import setup, find_packages
 setup(
     name="pdf_hunter",
     version="0.1.0",
-
     author="Simon Garisch",
-    author_email="gatman946 at gmail.com",
-
+    author_email="gatman946@gmail.com",
     description="Download PDF links from a webpage",
     long_description=open("README.md").read(),
-
-    packages=find_packages(exclude=('tests',)),
-    install_requires=[
-        "requests>=2.22.0",
-        "beautifulsoup4>=4.8.1"
-    ],
+    packages=find_packages(exclude=("tests",)),
+    install_requires=["requests>=2.22.0", "beautifulsoup4>=4.8.1"],
 )
